5b3a98a8d996dabd6a32706c318029e836c561a8
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 13:43:15 -0300

    Add files via upload

---
+++ Desafio022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite  seu nome completo : ')).strip()
+print("Analisando  seu nome...")
+print('Seu nome em Maiúscula é : {}'.format(nome.upper()))
+print('Seu nome em minúscula é : {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e  ele tem {} letras'.format(separa[0], len(separa[0])))
